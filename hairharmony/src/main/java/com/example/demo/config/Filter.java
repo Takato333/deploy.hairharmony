a6a8e4d7fb9a1cc3c63ab7c3cd10ab6941bd14e6
Takato333 <vanphuquocthinh2004@gmail.com>
2024-11-05 20:36:02 +0700

    s

--- hairharmony/src/main/java/com/example/demo/config/Filter.java
+++ hairharmony/src/main/java/com/example/demo/config/Filter.java
@@ -40,7 +40,8 @@ public class Filter extends OncePerRequestFilter {
             "/api/login",
             "/api/register",
             "/api/forgot-password",
-            "/api/login-google"
+            "/api/login-google",
+            "/api/administrator"
     );
 
     public boolean checkIsPublicAPI(String uri){
