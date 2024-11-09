29468f6696250f85a6f2350daf6299460fcf9916
Văn Phú Quốc Thịnh <vanphuquocthinh2004@gmail.com>
2024-11-09 19:28:44 +0700

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
                 .allowedOrigins("https://fe-hairsalon.vercel.app/") // Allow your frontend
+                .allowedOrigins("http://localhost:5173/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
